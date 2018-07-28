3fea1da1e13d6e52fa73c1d9ddc1a743ea7c8528
Burhan <burhan@kukapps.com>
2018-07-28 14:37:06 +0300

    Initial commit

---
+++ PlacesApp/PlacesApp/ViewControllers/PlaceDetailPopupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceDetailPopupViewController.swift
+//  PlacesApp
+//
+//  Created by Burhan on 28/07/2018.
+//  Copyright © 2018 Burhan. All rights reserved.
+//
+
+import UIKit
+
+class PlaceDetailPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
